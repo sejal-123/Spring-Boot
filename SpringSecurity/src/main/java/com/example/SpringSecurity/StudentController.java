6fe3aae0e9db4bf3b8ad04d00443a66e4c80455e
sejal-123 <sejalbudhani@gmail.com>
2025-03-13 00:28:58 +0530

    Added csrf token login and how to customize security filter chain

--- SpringSecurity/src/main/java/com/example/SpringSecurity/StudentController.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/StudentController.java
@@ -1,11 +1,34 @@
 package com.example.SpringSecurity;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    @Autowired
+    private StudentService service;
+
     @GetMapping("/students")
-    public
+    public List<Student> getStudents() {
+        return service.getStudents();
+    }
+
+    @PostMapping("/students")
+    public Boolean addStudent(@RequestBody Student student) {
+        return service.addStudent(student);
+    }
+
+    @GetMapping("/csrf_token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+
+    }
 }
---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.SpringSecurity;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Sejal", 30),
+            new Student(2, "Varun", 35)
+    ));
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public Boolean addStudent(Student student) {
+        return students.add(student);
+    }
+}
