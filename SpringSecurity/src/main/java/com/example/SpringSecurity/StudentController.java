b222a780dc7cbb1c9d15eb87bb6192181022c219
sejal-123 <sejalbudhani@gmail.com>
2025-03-12 23:47:35 +0530

    Added student class

---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/StudentController.java
@@ -0,0 +1,11 @@
+package com.example.SpringSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/students")
+    public
+}
