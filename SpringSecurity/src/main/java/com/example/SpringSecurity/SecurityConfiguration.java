9eca5376f1f3435eeb12f4ddebfec77c0f159131
sejal-123 <sejalbudhani@gmail.com>
2025-03-13 23:18:16 +0530

    Added inMemoryUserDetailsManager in UserDetailsService

--- SpringSecurity/src/main/java/com/example/SpringSecurity/SecurityConfiguration.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/SecurityConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +25,14 @@ public class SecurityConfiguration {
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withDefaultPasswordEncoder().username("seji").password("s@123").roles("USER").build();
+        UserDetails user2 = User.withDefaultPasswordEncoder().username("varun").password("v@123").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+
     }
 }
