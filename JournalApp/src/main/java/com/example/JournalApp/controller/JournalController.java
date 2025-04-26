befd5ddb7d2fc9e6929bef69082519522ffd8386
sejal-123 <sejalbudhani@gmail.com>
2025-04-26 20:36:37 +0530

    Added methods for journal CRUD in journal service

--- JournalApp/src/main/java/com/example/JournalApp/controller/JournalController.java
+++ JournalApp/src/main/java/com/example/JournalApp/controller/JournalController.java
@@ -1,16 +1,17 @@
 package com.example.JournalApp.controller;
 
 import com.example.JournalApp.model.Journal;
+import com.example.JournalApp.model.User;
 import com.example.JournalApp.service.JournalService;
+import com.example.JournalApp.service.UserService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/journal")
@@ -19,47 +20,81 @@ public class JournalController {
     @Autowired
     private JournalService journalService;
 
-    @GetMapping("")
-    public ResponseEntity<List<Journal>> getAllJournals() {
-        List<Journal> journals = journalService.getAll();
-        return new ResponseEntity<>(journals, HttpStatus.OK);
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{username}")
+    public ResponseEntity<List<Journal>> getAllJournals(@PathVariable String username) {
+        User user = userService.findByUserName(username);
+        if (user != null) {
+            List<Journal> journals = user.getJournalEntries();
+            return new ResponseEntity<>(journals, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("")
-    public String createEntry(@RequestBody Journal journal) {
-//        if (journalMap.get(journal.getId()) == null) {
-//            journalMap.put(journal.getId(), journal);
-//            return "Success";
-//        } else {
-//            return "Record already exists";
-//        }
+    @PostMapping("/{username}")
+    public ResponseEntity<Journal> createEntry(@RequestBody Journal journal, @PathVariable String username) {
+        try {
+            User user = userService.findByUserName(username);
+            if (user != null) {
+                journalService.saveEntry(journal, username);
+                return new ResponseEntity<>(journal, HttpStatus.CREATED);
+            }
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return null;
     }
 
-    @GetMapping("/{id}")
-    public Journal getJournal(@PathVariable long id) {
+    @GetMapping("/{username}/{id}")
+    public ResponseEntity<?> getJournal(@PathVariable ObjectId id, @PathVariable String username) {
+        User user = userService.findByUserName(username);
+        if (user != null) {
+            List<Journal> collect = user.getJournalEntries().stream().filter(x -> x.getId().equals(id)).collect(Collectors.toList());
+            if(!collect.isEmpty()) {
+                Optional<Journal> journal = journalService.findById(id);
+                if (journal.isPresent()) {
+                    return new ResponseEntity<>(journal, HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{username}/{id}")
+    public ResponseEntity<?> updateJournal(@PathVariable String username, @PathVariable ObjectId id, @RequestBody Journal journal) {
+        try {
+            User user = userService.findByUserName(username);
+            if (user != null) {
+                List<Journal> collect = user.getJournalEntries().stream().filter(x -> x.getId().equals(id)).collect(Collectors.toList());
+                if(!collect.isEmpty()) {
+                    Optional<Journal> old = journalService.findById(id);
+                    if (old.isPresent()) {
+                        Journal entry = old.get();
+                        entry.setTitle(journal.getTitle() != null && !journal.getTitle().equals("") ? journal.getTitle() : entry.getTitle());
+                        entry.setSummary(journal.getSummary() != null && !journal.getSummary().equals("") ? journal.getSummary() : entry.getSummary());
+                        journalService.updateEntry(entry);
+                        return new ResponseEntity<>(entry, HttpStatus.OK);
+                    }
+                }
+            }
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return null;
     }
 
-    @PutMapping("/{id}")
-    public String updateJournal(@PathVariable long id, @RequestBody Journal journal) {
-//        if (journalMap.get(id) != null) {
-//            journalMap.put(id, journal);
-//            return "Updated";
-//        } else {
-//            return "Record doesn't exists";
-//        }
-        return null;
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteJournal(@PathVariable long id) {
-//        if (journalMap.get(id) != null) {
-//            journalMap.remove(id);
-//            return "Deleted";
-//        } else {
-//            return "Record doesn't exists";
-//        }
+    @DeleteMapping("/{username}/{id}")
+    public ResponseEntity<?> deleteJournal(@PathVariable String username, @PathVariable ObjectId id) {
+        User user = userService.findByUserName(username);
+        if (user != null) {
+            boolean removed = journalService.delete(id, username);
+            if (removed) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         return null;
     }
 
--- JournalApp/src/main/java/com/example/JournalApp/service/JournalService.java
+++ JournalApp/src/main/java/com/example/JournalApp/service/JournalService.java
@@ -1,11 +1,16 @@
 package com.example.JournalApp.service;
 
 import com.example.JournalApp.model.Journal;
+import com.example.JournalApp.model.User;
 import com.example.JournalApp.repository.JournalRepo;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JournalService {
@@ -13,11 +18,51 @@ public class JournalService {
     @Autowired
     private JournalRepo journalRepo;
 
+    @Autowired
+    private UserService userService;
+
     public Journal createJournal(Journal journal) {
         return journalRepo.save(journal);
     }
 
     public List<Journal> getAll() {
         return journalRepo.findAll();
+    }
+
+    public Optional<Journal> findById(ObjectId id) {
+        return journalRepo.findById(id);
+    }
+
+    @Transactional
+    public void saveEntry(Journal journal, String username) {
+        try {
+            User user = userService.findByUserName(username);
+            journal.setDate(LocalDateTime.now());
+            Journal saved = journalRepo.save(journal);
+            user.getJournalEntries().add(saved);
+            userService.updateUser(user);
+        } catch(Exception e) {
+            throw new RuntimeException("An error occured while saving entry", e);
+        }
+    }
+
+    public Journal updateEntry(Journal entry) {
+        return journalRepo.save(entry);
+    }
+
+    @Transactional
+    public boolean delete(ObjectId id, String username) {
+        boolean removed = false;
+        try {
+            User user = userService.findByUserName(username);
+            removed = user.getJournalEntries().removeIf(x-> x.getId().equals(id));
+            if (removed) {
+                userService.updateUser(user);
+                journalRepo.deleteById(id);
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("An error occured while removing", e);
+        }
+        return removed;
     }
 }
