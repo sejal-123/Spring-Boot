c74b2ef4733a8e08cb13417aa9a377b9a6e77bb1
sejal-123 <sejalbudhani@gmail.com>
2025-05-02 22:33:59 +0530

    Added smtp email service

--- JournalApp/src/main/java/com/example/JournalApp/repository/UserRepoImpl.java
+++ JournalApp/src/main/java/com/example/JournalApp/repository/UserRepoImpl.java
@@ -1,6 +1,7 @@
 package com.example.JournalApp.repository;
 
 import com.example.JournalApp.model.User;
+import com.example.JournalApp.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -15,12 +16,18 @@ public class UserRepoImpl {
     @Autowired
     MongoTemplate mongoTemplate;
 
+    @Autowired
+    EmailService emailService;
+
     public List<User> getUserSA() {
         Query query = new Query();
 //        query.addCriteria(Criteria.where("email").exists(true));
         query.addCriteria(Criteria.where("email").ne("").ne(null));
         query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
         List<User> users = mongoTemplate.find(query, User.class);
+        for (User user: users) {
+            emailService.sendEmail(user.getEmail(), "Test mail", "Hello, How are you?");
+        }
         return users;
     }
 }
